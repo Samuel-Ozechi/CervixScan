3c8b9ff397f1f35be0da09edee93009a6379a396
Samuel Ozechi <ozechisamuel@gmail.com>
2026-05-02 13:34:56 +0000

    added requirements.txt to backend

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='src',
     packages=find_packages(),
     version='0.1.0',
-    description='Development of mobile and web applications that utilize  a built deep learning based algorithm to accurately identify a woman's cervix type ased on images.',
+    description="Development of mobile and web applications that utilize  a built deep learning based algorithm to accurately identify a woman's cervix type ased on images.",
     author='Samuel Ozechi',
     license='MIT',
 )
